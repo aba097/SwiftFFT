fcf1084250eb85f721b5b32d2255ea1ac28824bd
aba <socceraba2701@gmail.com>
2021-09-26 16:44:01 +0900

    ViewController

---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Tmp
+//
+//  Created by 相場智也 on 2021/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let inputmic = InputMic(inMaxFramesPerSlice: 4096)
+        
+        //録音する
+        inputmic.startRecord()
+        
+        //録音終了する
+        //inputmic.stopRecord()
+        
+        //Info.plistにPrivacy - Microphone Usage Description を追加する
+        
+    }
+
+
+}
+
